a63614cc46774602d2a94ca29ee3d77ec96573a5
karan <karansawan5050@gmail.com>
2023-09-26 23:12:24 +0530

    typo fix thired

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,14 +6,14 @@ import { navVariants } from '../utils/motion';
 
 const Navbar = () => (
   <motion.nav
-    variants={ navVariants }
+    variants={navVariants}
     initial="hidden"
     whileInView="show"
-    viewport={ {once : true} }
-    className={ `${styles.xPaddings} py-8 relative` }
+    viewport={{ once : true }}
+    className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className={ `${styles.innerWidth} mx-auto flex justify-between gap-8` }>
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img
         src="/search.svg"
         alt="search"
